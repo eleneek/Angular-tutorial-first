f45c142666bd9bfbbaf273a1ac78030c5d5471e1
eleneek <elene.kvaratskhelia.1@btu.edu.ge>
2019-07-09 20:45:58 +0400

    feat(17-observable-rxjs-part-3): Finish tutorial

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -23,14 +23,22 @@ export class CurrencyComponent implements OnInit {
      this.length = this.rates.length;
    };
 
-   const filterCallback = ({ value }) => value > 10;
+   const filterCallback = ({ value }) => value > 50;
 
-   const mapCallback = ({currency, value}) => ({icon : '🏦', currency, value});
+   const mapCallback = ({rates}) => {
+     const currency = Object.keys(rates)[0]
+     const value = rates[currency]
+     return {
+       icon:  '🏦',
+       currency,
+       value
+     };
+    };
 
    const Observer = this.currencyService.Observer;
 
    const observable = Observer
-   .pipe(filter(filterCallback), map(mapCallback))
+   .pipe(map(mapCallback), filter(filterCallback))
    .subscribe( {next: action, complete});
  }
 }
